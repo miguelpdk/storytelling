fd26ece4b8106fceb662f7c837365617aaee23c1
miguelpdk <160019309+miguelpdk@users.noreply.github.com>
2024-10-09 08:45:25 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -22,7 +22,7 @@ async function quantidadeUsuariosPorRede() {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'Redes sociais com mais usuários',
+            text: 'Minhas notas',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
